fdb4bb9abb288f25a4266d1b9c71b91c7348265d
Diana Diaz <93556710+WinterLanterns@users.noreply.github.com>
2022-10-24 23:08:36 -0700

    finnished

---
+++ js/script.js
@@ -0,0 +1,15 @@
+window.addEventListener("scroll", reveal);
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowheight = window.innerHeight;
+    var revealTop = reveals[i].getBoundingClientRect().top;
+    var revealPoint = 150;
+    if (revealTop < windowheight - revealPoint) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
